cd9f2a6b6d4d4b5b890e386132236ec68d09d3f8
7c00 <i@7c00.cc>
2021-12-16 17:06:54 +0800

    Update

--- src/get-release.js
+++ src/get-release.js
@@ -1,10 +1,10 @@
-const core = require("@actions/core");
+const { setOutput, getInput } = require("@actions/core");
 const { GitHub, context } = require("@actions/github");
 
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const github = new GitHub(getInput("token", { required: true }));
 
     // Get owner and repo from context of payload that triggered the action
     const { owner, repo } = context.repo;
@@ -13,7 +13,7 @@ async function run() {
     const tagName = context.ref;
 
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
-    const tag = tagName.replace("refs/tags/", "");
+    const tag = getInput("tag") ?? tagName.replace("refs/tags/", "");
 
     // Get a release from the tag name
     // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
@@ -21,31 +21,41 @@ async function run() {
     const getReleaseResponse = await github.repos.getReleaseByTag({
       owner,
       repo,
-      tag
+      tag,
     });
 
     // Get the outputs for the created release from the response
     const {
-      data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl, name: name, body: body, draft: draft, prerelease: prerelease, author: author }
+      data: {
+        id: releaseId,
+        html_url: htmlUrl,
+        upload_url: uploadUrl,
+        name: name,
+        body: body,
+        draft: draft,
+        prerelease: prerelease,
+        author: author,
+      },
     } = getReleaseResponse;
 
-    console.log(`Got release info: '${releaseId}', '${htmlUrl}', '${uploadUrl}', '${name}', '${draft}', '${prerelease}', '${body}', '${author}'`);
+    console.log(
+      `Got release info: '${releaseId}', '${htmlUrl}', '${uploadUrl}', '${name}', '${draft}', '${prerelease}', '${body}', '${author}'`
+    );
 
     // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    core.setOutput("id", releaseId.toString());
-    core.setOutput("html_url", htmlUrl);
-    core.setOutput("upload_url", uploadUrl);
-    core.setOutput("tag_name", tag);
-    core.setOutput("name", name);
-    core.setOutput("body", body);
-    core.setOutput("draft", draft);
-    core.setOutput("prerelease", prerelease);
-    core.setOutput("author", author);
+    setOutput("id", releaseId.toString());
+    setOutput("html_url", htmlUrl);
+    setOutput("upload_url", uploadUrl);
+    setOutput("tag_name", tag);
+    setOutput("name", name);
+    setOutput("body", body);
+    setOutput("draft", draft);
+    setOutput("prerelease", prerelease);
+    setOutput("author", author);
   } catch (error) {
     console.log(error);
-    core.setFailed(error.message);
+    setFailed(error.message);
   }
 }
 
 module.exports = run;
-
